e2c6c353dbae506b712e152e628a7ddba80be936
vivekananda69 <vivekanandabandapu1@gmail.com>
2026-06-11 01:29:03 +0530

    Updated QR menu URL

--- pages/1_Admin_Panel.py
+++ pages/1_Admin_Panel.py
@@ -546,10 +546,8 @@ with tab4:
 
             menu_url = st.text_input(
                 "Menu URL",
-                value=st.secrets.get(
-                    "APP_URL",
-                    "https://digitalrestaurant-menu-keuve2cmjlwjwylvpy4arv.streamlit.app"
-                ),
+                value="ttps://digitalrestaurant-menu-keuve2cmjlwjwylvpy4arv.streamlit.app/Customer_Menu",
+                # help="When deployed, replace this with your Streamlit Cloud URL.",
             )
             qr_color    = st.color_picker("QR Code Color",    "#1A1A2E")
             bg_color    = st.color_picker("Background Color", "#FFFFFF")
